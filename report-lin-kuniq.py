23aea77f0e6bccd5d54c7dc11bf6479ab9ccc21b
parul-sharma <parul15123@iiitd.ac.in>
2023-06-23 14:09:10 -0400

    adding scripts

---
+++ report-lin-kuniq.py
@@ -0,0 +1,164 @@
+#!/bin/python
+
+'''USAGE: ./report-lin.py lingroups.txt taxonomy/data.txt input.report'''
+
+import sys
+import pandas as pd
+import argparse
+
+#Function to convert string to List
+def convert_LIN_string(LIN):
+	LIN_string=LIN.split(",")
+	return LIN_string
+
+
+def find_taxid_of_lingroup(lingroup, data):
+	similarLIN = [i for i in data['LIN'] if lingroup in i] #using list comprehension to get string with substring 
+	top_LIN=similarLIN[0] #select the first best match
+	index = data[data['LIN']==top_LIN].index.values #indeex of top LIN
+	index = int(index)
+	taxids = data['taxid_LIN'][index] # taxids of the top LIN
+	taxids = convert_LIN_string(taxids)
+	lingroup = convert_LIN_string(lingroup)
+	LINgroup_taxids = taxids[0:len(lingroup)] #taxids of the lingroup
+	#print(LINgroup_taxids)
+	#print(len(lingroup))
+	return LINgroup_taxids
+
+def cumulative_sum(taxids, in_file):
+	reads=0
+	last_taxid = taxids[-1]
+	#print(last_taxid)
+	index = in_file[in_file[4] == int(last_taxid)].index.values
+	if index.size >0:
+		index = int(index)
+		reads = in_file[1][index]
+	#print(reads)
+	return reads	
+#	cumsum = 0
+#	for i in range(len(taxids)):
+#		#print(taxids[i])
+#		index = in_file[in_file[4] == int(taxids[i])].index.values #index of match 
+#		#print(index)
+#		if index.size > 0:
+#			index = int(index)
+#			#print(index)
+#			cumsum = cumsum + in_file[2][index]
+#	#print(cumsum)
+#	return cumsum
+		
+def assigned_reads(taxids, in_file):
+	reads=0
+	last_taxid = taxids[-1]
+	#print(last_taxid)
+	index = in_file[in_file[4] == int(last_taxid)].index.values
+	if index.size >0:
+		index = int(index)
+		reads = in_file[2][index] 
+	#print(reads)
+	return reads
+
+def total_reads_count(taxids,in_file):
+	reads=0
+	taxid = taxids[0] #taxid of the first tax rank; root of all rssc genomes
+	#print(taxid)
+	index = in_file[in_file[4] == int(taxid)].index.values
+	#print(index)
+	if index.size >0:
+		index = int(index)
+		reads = in_file[1][index]
+	#print(reads)
+	return reads
+
+def total_reads_length(taxids, in_output):
+	read_length=0
+	last_taxid = taxids[-1]
+	#print(last_taxid)
+	index = in_output[in_output[2] == int(last_taxid)].index.values
+	#print(index)
+	if index.size >0:
+		for i in range(len(index)):
+			curr_index = int(index[i])
+			if '|' in str(in_output[3][curr_index]): ####in case of paired reads eg: 151|149 use only first read length
+				read_length = read_length + int(in_output[3][curr_index].split('|')[0])
+			else:
+				read_length = read_length + in_output[3][curr_index]
+	#print(reads)
+	return read_length
+
+def  main():
+	p = argparse.ArgumentParser()
+	p.add_argument('--lin_file', help = 'txt file contaning lingroup names and prefixes')
+	p.add_argument('--data_file', help = 'txt file containing the taxonom details produced in the db construction step')
+	p.add_argument('--in_file_report', help = 'report output generated from kraken2 (condensed output file)')
+	p.add_argument('--in_file_output', help = 'default output generated from kraken2 ( output file)')
+	p.add_argument('--output')
+	args = p.parse_args()
+
+	lingroup_file = args.lin_file
+	#reading the lin file
+	lin_file = pd.read_csv(lingroup_file,sep='\t')
+	
+	data_file = args.data_file
+        #reading the file      
+	data = pd.read_csv(data_file,sep='\t')
+	#print(type(data.loc[0, 'taxid_LIN']))
+	data['taxid_LIN'] = data['taxid_LIN'].str.strip('[]')
+	data['parent_LIN'] = data['parent_LIN'].str.strip('[]')
+	#print(data)
+
+	in_file_orig = args.in_file_report
+        #reading the file
+	in_file_orig = pd.read_csv(in_file_orig,sep='\t',header=None,index_col=False,skiprows=3)
+	in_file = in_file_orig.iloc[:,[0,1,2,3,6]]
+	in_file.columns = range(in_file.columns.size)
+
+	in_output = args.in_file_output
+	#reading the file
+	in_output = pd.read_csv(in_output,sep='\t',header=None,index_col=False)
+	
+	#open output file for report
+	#output = open(args.output, 'w')
+	out_file = pd.read_csv(lingroup_file,sep='\t')
+	#out_file['LINgroup_Name'] = ''
+	#out_file['LINgroup_prefix'] = ''
+	out_file['Assigned_reads'] = ''
+	out_file['Percentage_assigned_reads'] = ''
+	out_file['Unique_Assigned_reads'] = ''
+	out_file['Percentage_unique_assigned_reads'] = ''
+	out_file['Total_reads_length'] = ''
+	temp_taxid = find_taxid_of_lingroup(lin_file['LINgroup_prefix'][1], data) ##to find the first taxid of all
+	#print(temp_taxid)
+	total_reads=total_reads_count(temp_taxid, in_file)
+	#print(total_reads)
+	i=0
+	while (i < len(lin_file['LINgroup_Name'])) and (total_reads is not 0) :
+		#find list of taxids associated with best match of LINgroup
+		taxid = find_taxid_of_lingroup(lin_file['LINgroup_prefix'][i], data)
+		
+		cumulative_reads = cumulative_sum(taxid, in_file)
+		unique_reads = assigned_reads(taxid, in_file)
+		reads_length = total_reads_length(taxid, in_output)
+		#adding the result to the output file
+		out_file['Assigned_reads'][i] = cumulative_reads
+		out_file['Percentage_assigned_reads'][i] = (cumulative_reads/total_reads)*100
+		out_file['Unique_Assigned_reads'][i] = unique_reads
+		out_file['Percentage_unique_assigned_reads'][i] = (unique_reads/total_reads)*100
+		out_file['Total_reads_length'][i] = reads_length
+		i = i + 1
+
+	#print(out_file)
+	#out_file = open(out_file, 'w')
+	#output = out_file.copy()
+	df = pd.DataFrame({"LINgroup_Name":['Total_reads'], "Assigned_reads": [total_reads]})
+	#out_file.append('LINgroup_Name': 'Total_reads', 'Cumulative_reads': str(in_file[1][1]))
+	#print(out_file)
+	out_file = pd.concat([out_file,df])
+	print(out_file)
+	out_file.to_csv(args.output, sep=',', index=False)	
+
+if __name__=='__main__':
+	main()
+
+
+
---
+++ sankey_plot.R
@@ -0,0 +1,67 @@
+if(!require(networkD3)) install.packages("networkD3", repos = "http://cran.us.r-project.org")
+library(networkD3)
+args <- commandArgs(trailingOnly=TRUE)
+
+if (length(args) != 1) {
+  cat("Usage: Rscript sankey_plot.R input_file\n Add complete path to input file!")
+  quit(save = "no", status = 1)
+}
+
+input_file <- args[1]
+input <- read.delim(input_file)
+
+##input <- read.delim("~/Desktop/LIN-kraken/Ralstonia/Sankey-plots/brc1_c0.finalLINoutput.txt")
+
+###Getting the list of names in the data:
+
+
+a=input$LINgroup_Name  ##stores all the LINgroups in a list
+a=as.character(a)
+name=unlist(strsplit(a, ';'))  #stores all the broken down list of LINgroup categories by splitting on ';'
+
+##only retain the unique LINgroups categories
+name = unique(name)
+nodes = data.frame(name)
+
+###creating a dictionary of 'name' to help create source and target lists 
+vals <- 0:length(name)
+keys <- name
+##linking vals to keys in R is done using names function..python equivalent of dict
+names(vals) <-keys
+
+#####finding source and target and value lists
+source = c()
+target = c()
+value = c()
+for (i in 1:nrow(input))
+{ 
+  x=as.list(unlist(strsplit(as.character(input[i,'LINgroup_Name']),';')))
+  x
+  if(length(x) > 1)
+  { source = append(source, vals[[x[[length(x)-1]]]])  ##adding corresponding int value using vals dictionary 
+    target = append(target, vals[[x[[length(x)]]]])  ##adding corresponding int value using vals dictionary 
+    value = append(value, input[i,'Assigned_reads'])
+  } 
+  #else    ###this will be the case of total reads with only 1 category of LINgroups
+ # { source = vals[x[-1]]  ##adding corresponding int value using vals dictionary 
+  #  target = vals[x[-1]]  ##adding corresponding int value using vals dictionary 
+  #  value = input[i,'Assigned_reads']
+  #}
+}  
+
+
+links = data.frame(source,target,value)
+##Remove rows from df where abundance is zero!!
+links = links[links$value != 0, ]
+##order the df by source so the linkage is proper
+links = links[order(links$source),]  ##this introduced NA in the dataframe
+###links = links[links$source != NA, ]  ##cleaning NAs
+
+
+df=list(nodes,links)
+q <- sankeyNetwork(Links = links, Nodes = nodes, Source = "source",
+                   Target = "target", Value = "value", NodeID = "name",
+                   units = "", fontSize = 12, nodeWidth = 30, sinksRight = FALSE)
+
+
+saveNetwork(q, "plot.html")
